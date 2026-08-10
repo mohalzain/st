105444f028d7955ae85d2ba146048351ec39f9d1
Mohammed Abdullah <mohammeddalzain@gmail.com>
2026-08-10 20:21:13 +0300

    added backend template

--- backend.js
+++ backend.js
@@ -1,1 +1,24 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import dotenv from 'dotenv'
+
+
+dotenv.config({path:'./enviroment.env'})
+const path = process.cwd()
+const app = express()
+
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+
+
+
+
+
+
+app.listen(process.env.PORT,process.env.ADDRESS,(error)=>{
+    if (error) {
+        console.log(`An Error Occured:\n ${error}`)
+    }
+    console.log(`Server Running On: ${process.env.ADDRESS}:${process.env.PORT}`)
+})
